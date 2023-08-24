61c3feffc0e4e2283c70f545dd3e7a59eb317db6
msafnan <msmohamedafnan17@gmail.com>
2023-08-24 17:33:31 +0530

    Afnan background changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div class="bg-[#181028] flex justify-center items-center min-h-screen ">
+      <div class="bg-[#6b7280] flex justify-center items-center min-h-screen ">
         <div class=" bg-white p-4 rounded-xl m-32">
           <div class="flex overflow-x-auto space-x-4 p-4">
-
             <div class="flex-shrink-0 w-1/3 shadow border rounded p-4">
               <h1>Mohammed Taha</h1>
               <p>A new message from Mohammed tuaha awaits you</p>
@@ -28,7 +27,6 @@ function App() {
               <h1>Mohammed Taha</h1>
               <p>A new message from Mohammed tuaha awaits you</p>
             </div>
-
           </div>
         </div>
       </div>
