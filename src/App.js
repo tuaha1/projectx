1a5f50cd3b2e0d8c03264bddadd3358b0b58b612
tuaha1 <tuahakst@gmail.com>
2023-08-24 18:22:37 +0530

    change

--- src/App.js
+++ src/App.js
@@ -4,22 +4,15 @@ function App() {
   return (
     <div className="App">
       <div class="bg-[#181028] flex justify-center items-center min-h-screen ">
-        <div class=" bg-white p-4 rounded-xl m-32">
-          <div class="flex overflow-x-auto space-x-4 p-4">
+        <div class=" bg-white p-4 rounded-xl m-24">
 
-            <div class="flex-shrink-0 w-1/3 shadow border rounded p-4">
-              <h1>Mohammed Taha</h1>
-              <p>A new message from Mohammed tuaha awaits you</p>
+          <div class="flex space-x-4 ">
+
+            <div class="flex-shrink-0 w-1/3 shadow border rounded p-4 text-left">
+              <h1 className='text-xl'>Mohammed Taha</h1>
+              <p className='text-slate-500'>A new message from Mohammed tuaha awaits you</p>
             </div>
 
-            <div class="flex-shrink-0 w-1/3 shadow border rounded p-4">
-              <h1>Mohammed Taha</h1>
-              <p>A new message from Mohammed tuaha awaits you</p>
-            </div>
-            <div class="flex-shrink-0 w-1/3 shadow border rounded p-4">
-              <h1>Mohammed Taha</h1>
-              <p>A new message from Mohammed tuaha awaits you</p>
-            </div>
             <div class="flex-shrink-0 w-1/3 shadow border rounded p-4">
               <h1>Mohammed Taha</h1>
               <p>A new message from Mohammed tuaha awaits you</p>
