5b75d51f5d19fb2ea4d667d6d465968915c73988
msafnan <msmohamedafna17@gmail.com>
2023-08-25 15:51:52 +0530

    Afnan customer registration

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 
 import "./App.css";
 import Login from "./components/login/login";
+import CustomerRegistration from "./components/customer registration/customerRegistrstion";
 
 function App() {
   return (
     <div className="App">
       <Login />
+      <CustomerRegistration />
     </div>
   );
 }
--- src/components/customer registration/customerRegistrstion.js
+++ src/components/customer registration/customerRegistrstion.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 
-const Login = () => {
+const CustomerRegistration = () => {
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Log in to your account
+          Customer Registration
         </h2>
       </div>
 
@@ -34,14 +34,6 @@ const Login = () => {
               >
                 Password
               </label>
-              <div className="text-sm">
-                <a
-                  href="#"
-                  className="font-semibold text-indigo-600 hover:text-indigo-500"
-                >
-                  Forgot password?
-                </a>
-              </div>
             </div>
             <div className="mt-2">
               <input
@@ -50,6 +42,33 @@ const Login = () => {
                 type="password"
                 autoComplete="current-password"
                 required
+                className="pl-2 ...block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <label className="block text-sm font-medium flex items-center justify-between">
+              Phone
+            </label>
+            <div className="mt-2">
+              <input
+                id="Phone"
+                name="Phone"
+                type="number"
+                required
+                className="pl-2 ...block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <label className="block text-sm font-medium flex items-center justify-between">
+              Email
+            </label>
+            <div className="mt-2">
+              <input
+                id="Email"
+                name="Email"
+                type="Email"
                 className="pl-2 ...block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
@@ -64,19 +83,9 @@ const Login = () => {
             </button>
           </div>
         </form>
-
-        <p className="mt-10 text-center text-sm text-gray-500">
-          Not a member?{" "}
-          <a
-            href="#"
-            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-          >
-            Start a 14 day free trial
-          </a>
-        </p>
       </div>
     </div>
   );
 };
 
-export default Login;
+export default CustomerRegistration;
