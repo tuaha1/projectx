c64c545cf66d6d1400a236152e0a6a62680652b5
msafnan <msmohamedafna17@gmail.com>
2023-08-25 16:24:15 +0530

    Afnan profile registration

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React, { useState } from "react";
 import "./App.css";
 import Login from "./components/login/login";
 import CustomerRegistration from "./components/customer registration/customerRegistrstion";
+import ProfileRegistration from "./components/profile registratio/profileRegistration";
 
 function App() {
   return (
     <div className="App">
       <Login />
       <CustomerRegistration />
+      <ProfileRegistration />
     </div>
   );
 }
--- src/components/profile registratio/profileRegistration.js
+++ src/components/profile registratio/profileRegistration.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 
-const CustomerRegistration = () => {
+const ProfileRegistration = () => {
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Customer Registration
+          Profile Registration
         </h2>
       </div>
 
@@ -62,13 +62,78 @@ const CustomerRegistration = () => {
           </div>
           <div>
             <label className="block text-sm font-medium flex items-center justify-between">
-              Email
+              Age
             </label>
             <div className="mt-2">
               <input
-                id="Email"
-                name="Email"
-                type="Email"
+                id="Age"
+                name="Age"
+                type="Number"
+                className="pl-2 ...block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <label className="block text-sm font-medium flex items-center justify-between">
+              Gender
+            </label>
+            <select
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6
+                block text-sm font-medium flex items-center justify-between"
+              data-te-select-init
+            >
+              <option value="1">Male</option>
+              <option value="2">Female</option>
+            </select>
+          </div>
+          <div>
+            <label className="block text-sm font-medium flex items-center justify-between">
+              Skills
+            </label>
+            <div className="mt-2">
+              <input
+                id="Skills"
+                name="Skills"
+                type="array"
+                className="pl-2 ...block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <label className="block text-sm font-medium flex items-center justify-between">
+              Education
+            </label>
+            <div className="mt-2">
+              <input
+                id="Education"
+                name="Education"
+                type="text"
+                className="pl-2 ...block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <label className="block text-sm font-medium flex items-center justify-between">
+              Address
+            </label>
+            <div className="mt-2">
+              <input
+                id="Address"
+                name="Address"
+                type="Text"
+                className="pl-2 ...block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <label className="block text-sm font-medium flex items-center justify-between">
+              Occupation
+            </label>
+            <div className="mt-2">
+              <input
+                id="Occupation"
+                name="Occupation"
+                type="Text"
                 className="pl-2 ...block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
@@ -88,4 +153,4 @@ const CustomerRegistration = () => {
   );
 };
 
-export default CustomerRegistration;
+export default ProfileRegistration;
