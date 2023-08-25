b128130f5f88b780e8c0eac4b145595f89fdca6e
msafnan <msmohamedafna17@gmail.com>
2023-08-25 15:26:27 +0530

    Afnan login edit

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -16,14 +16,13 @@ const Login = () => {
               htmlFor="email"
               className="block text-sm font-medium flex items-center justify-between"
             >
-              Email address
+              Username
             </label>
             <div className="mt-2">
               <input
-                id="email"
-                name="email"
-                type="email"
-                autoComplete="email"
+                id="Username"
+                name="Username"
+                type="text"
                 required
                 className="pl-2 ...block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
