b8d3c6d924635cd2d1b546e5d6a5ea0118de071e
msafnan <msmohamedafna17@gmail.com>
2023-08-25 10:06:16 +0530

    Afnan login

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
+import React, { useState } from "react";
+
 import "./App.css";
+import Login from "./components/login/login";
 
 function App() {
   return (
     <div className="App">
-      <div class="bg-[#6b7280] flex justify-center items-center min-h-screen ">
-        <div class=" bg-white p-4 rounded-xl m-32">
-          <div class="flex overflow-x-auto space-x-4 p-4">
-            <div class="flex-shrink-0 w-1/3 shadow border rounded p-4">
-              <h1>Mohammed Taha</h1>
-              <p>A new message Mohammed </p>
-            </div>
-
-            <div class="flex-shrink-0 w-1/3 shadow border rounded p-4">
-              <h1>Mohammed Taha</h1>
-              <p>A new message from Mohammed tuaha awaits you</p>
-            </div>
-            <div class="flex-shrink-0 w-1/3 shadow border rounded p-4">
-              <h1>Mohammed Taha</h1>
-              <p>A new message from Mohammed tuaha awaits you</p>
-            </div>
-            <div class="flex-shrink-0 w-1/3 shadow border rounded p-4">
-              <h1>Mohammed Taha</h1>
-              <p>A new message from Mohammed tuaha awaits you</p>
-            </div>
-            <div class="flex-shrink-0 w-1/3 shadow border rounded p-4">
-              <h1>Mohammed Taha</h1>
-              <p>A new message from Mohammed tuaha awaits you</p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Login />
     </div>
   );
 }
