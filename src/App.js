7d8fc5456fbbd424813042cae44c90540b75a5ad
msafnan <msmohamedafna17@gmail.com>
2023-08-28 11:51:54 +0530

    first change

--- src/App.js
+++ src/App.js
@@ -10,12 +10,12 @@ function App() {
     <div className="App">
       <div class="bg-[#181028] flex justify-center items-center min-h-screen ">
         <div class=" bg-white p-4 rounded-xl m-24">
-
           <div class="flex space-x-4 ">
-
             <div class="flex-shrink-0 w-1/3 shadow border rounded p-4 text-left">
-              <h1 className='text-xl'>Mohammed Taha</h1>
-              <p className='text-slate-500'>A new message from Mohammed tuaha awaits you</p>
+              <h1 className="text-xl">Mohammed Taha</h1>
+              <p className="text-slate-500">
+                A new message from Mohammed tuaha awaits you
+              </p>
             </div>
 
             <div class="flex-shrink-0 w-1/3 shadow border rounded p-4">
@@ -26,7 +26,6 @@ function App() {
               <h1>Mohammed Taha</h1>
               <p>A new message from Mohammed tuaha awaits you</p>
             </div>
-
           </div>
         </div>
       </div>
